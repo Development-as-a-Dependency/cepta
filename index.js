e5e9225bf3c7ebb369cae97095bebe571bb104bd
Ted <callmehspear@gmail.com>
2022-12-15 17:32:31 +0000

    lots of stufff

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
+const fs = require('fs-extra');
+const { exec } = require('child_process');
 
-console.log("Hello World!");
+console.log("Creating new project...");
+
+fs.copy(__dirname + '/bin', './', err =>{
+  if (err) {
+    console.log("Something went wrong when creating files.")
+    return;
+  }
+  console.log('Installing packages...')
+  exec('npm i', (err, stdout, stderr) => {
+    if (err) {
+      console.log("Something went wrong when installing packages. Please install them manually.")
+      return;
+    }
+  });
+});
