5fcede83d620a0cda9f4c51057bb26ff14620f72
Ted <callmehspear@gmail.com>
2022-12-15 17:56:34 +0000

    Package json project name stuff

--- index.js
+++ index.js
@@ -2,18 +2,41 @@
 const fs = require('fs-extra');
 const { exec } = require('child_process');
 
-console.log("Creating new project...");
-
-fs.copy(__dirname + '/bin', './', err =>{
-  if (err) {
-    console.log("Something went wrong when creating files.")
+// if the args are create or c, create a new project
+if (process.argv[2] === 'create' || process.argv[2] === 'c') {
+  // if the project name is not specified, throw an error
+  if (!process.argv[3]) {
+    console.log("Please specify a project name. Example: cepta create <project name>");
     return;
   }
-  console.log('Installing packages...')
-  exec('npm i', (err, stdout, stderr) => {
+  var projectName = process.argv[3].toLowerCase();
+  console.log("Creating new project " + projectName + "...");
+
+  fs.copy(__dirname + '/bin', './', err =>{
     if (err) {
-      console.log("Something went wrong when installing packages. Please install them manually.")
+      console.log("Something went wrong when creating files.")
       return;
     }
+
+    fs.readFile('package.json', 'utf8', function (err,data) {
+      if (err) {
+        return console.log(err);
+      }
+      var result = data.replace(/"name": "project",/g, '"name": "' + projectName + '",');
+    
+      fs.writeFile('package.json', result, 'utf8', function (err) {
+        if (err) return console.log(err);
+        console.log('Installing packages...')
+        exec('npm i', (err, stdout, stderr) => {
+          if (err) {
+            console.log("Something went wrong when installing packages. Please install them manually.")
+            return;
+          }
+        });
+        console.log("Project created successfully. You can now run 'npm start' to start the server.");
+      });
+    });
   });
-});
+} else {
+  console.log("The command you entered is not valid. Please use 'cepta create <project name>' or 'cepta c <project name>' to create a new project.");
+};
