20e987ea25ef4236cdf39397671f7e5307f8e625
Ted <callmehspear@gmail.com>
2023-03-04 20:49:52 +0000

    add chalk

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
 #!/usr/bin/env node
-const fs = require('fs-extra');
-const axios = require('axios');
-const { exec } = require('child_process');
+const fs = require("fs-extra");
+const axios = require("axios");
+const { exec } = require("child_process");
+const chalk = require("chalk");
 
 // if the args are create or c, create a new project
-if (process.argv[2] === 'create' || process.argv[2] === 'c') {
+if (process.argv[2] === "create" || process.argv[2] === "c") {
   // if the project name is not specified, throw an error
   if (!process.argv[3]) {
-    console.log("Please specify a project name. Example: cepta create <project name>");
+    console.log(chalk.red("Please specify a project name."));
     return;
   }
   var projectName = process.argv[3].toLowerCase();
-  console.log("Creating new project " + projectName + "...");
-
-  fs.copy(__dirname + '/bin', './', err =>{
+  console.log(chalk.blue("Creating project..."));
+  fs.copy(__dirname + "/bin", "./", (err) => {
     if (err) {
-      console.log("Something went wrong when creating files.")
+      console.log(chalk.red("Something went wrong when creating the project."));
       return;
     }
 
-    fs.readFile('package.json', 'utf8', function (err,data) {
+    fs.readFile("package.json", "utf8", function (err, data) {
       if (err) {
-        return console.log(err);
+        return console.log(chalk.red(err));
       }
-      var result = data.replace(/"name": "project",/g, '"name": "' + projectName + '",');
-    
-      fs.writeFile('package.json', result, 'utf8', function (err) {
-        if (err) return console.log(err);
-        console.log('Installing packages...')
-        exec('npm i', (err, stdout, stderr) => {
+      var result = data.replace(
+        /"name": "project",/g,
+        '"name": "' + projectName + '",'
+      );
+
+      fs.writeFile("package.json", result, "utf8", function (err) {
+        if (err) return console.log(chalk.red(err));
+        console.log(chalk.blue("Installing packages..."));
+        exec("npm i", (err, stdout, stderr) => {
           if (err) {
-            console.log("Something went wrong when installing packages. Please install them manually.")
+            console.log(
+              chalk.red("Something went wrong when installing packages.")
+            );
             return;
           }
         });
-        console.log("Project created successfully. You can now run 'npm start' to start the server.");
+        console.log(chalk.green("Project created successfully!"));
       });
     });
   });
-} else if (process.argv[2] === 'help' || process.argv[2] === 'h') {
+} else if (process.argv[2] === "help" || process.argv[2] === "h") {
   console.log("Usage: cepta <command> <options>");
   console.log("Commands:");
   console.log("  create, c: Create a new project");
@@ -59,23 +64,25 @@ if (process.argv[2] === 'create' || process.argv[2] === 'c') {
   console.log("    Examples:");
   console.log("      cepta update");
   console.log("      cepta u");
-} else if (process.argv[2] === 'version' || process.argv[2] === 'v') {
-  axios.get('https://registry.npmjs.org/cepta')
-  .then(function (response) {
+} else if (process.argv[2] === "version" || process.argv[2] === "v") {
+  axios.get("https://registry.npmjs.org/cepta").then(function (response) {
     // get the version from package.json
-    var version = require('./package.json').version;
-    console.log("Installed Version: " + version);
-    console.log("Latest Version: " + response.data['dist-tags'].latest);
-  })
-} else if (process.argv[2] === 'update' || process.argv[2] === 'u') {
-  console.log("Updating cepta...");
-  exec('npm i -g cepta', (err, stdout, stderr) => {
+    var version = require("./package.json").version;
+    console.log(chalk.yellow("Current Version: " + version));
+    console.log(
+      chalk.yellow("Latest Version: " + response.data["dist-tags"].latest)
+    );
+  });
+} else if (process.argv[2] === "update" || process.argv[2] === "u") {
+  console.log(chalk.blue("Updating cepta..."));
+  exec("npm i -g cepta", (err, stdout, stderr) => {
     if (err) {
-      console.log("Something went wrong when updating cepta. Please update it manually.")
+      console.log(chalk.red("Something went wrong when updating cepta."));
       return;
     }
-    console.log("Cepta updated successfully. You can now run 'cepta help' to see the help message.");
+    console.log(chalk.green("Cepta updated successfully!"));
   });
 } else {
-  console.log("The command you entered is not valid. Run 'cepta help' for help.");
-};
+  console.log(chalk.red("Invalid command."));
+  console.log("Use 'cepta help' to see the list of commands.");
+}
