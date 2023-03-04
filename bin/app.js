e14c38bc95efe137dc8c4f3fc9e7dabef70bb2f1
Ted <callmehspear@gmail.com>
2023-03-04 20:53:59 +0000

    update sutff

--- bin/app.js
+++ bin/app.js
@@ -4,18 +4,20 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+// Define routes
+
 const indexRouter = require('./routes/index');
 
 const app = express();
 
 app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static('public'));
 
+// Routes
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
@@ -25,11 +27,8 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
