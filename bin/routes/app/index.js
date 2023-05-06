e66721018a028548847cf99ae3bf4ea7f076803f
Teddy Brine <ted@daad.wtf>
2023-05-06 10:50:44 +0100

    yes

--- bin/routes/app/index.js
+++ bin/routes/app/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-var embed = require('../../src/embed');
-var func = require('../../src/func');
+const embed = require('../../src/embed');
+const func = require('../../src/func');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
   res.render('index', { title: 'Home', embed: embed });
 });
 
